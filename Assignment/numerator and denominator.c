21271172d7714ce718cae22851550cddcad982bf
sadiaamir-tech <sadia.amir2006@gmail.com>
2025-10-06 01:06:55 -0700

    commit

---
+++ Assignment/numerator and denominator.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+	float nume,deno;
+	printf("\nEnter the numerator:");
+	scanf("%f",&nume);
+	printf("\nEnter the denominator:");
+	scanf("%f",&deno);
+	if(deno==0)
+	printf("\nError! you can't take zero as denominator");
+	else
+	printf("The result of division is: %.2f",nume/deno);
+	return 0;
+}
+
