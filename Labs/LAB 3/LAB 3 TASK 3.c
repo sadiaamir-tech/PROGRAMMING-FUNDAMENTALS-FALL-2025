6a9757c40521a98a1aa15eaaf9b33a9541246ebb
sadiaamir-tech <sadia.amir2006@gmail.com>
2025-10-24 08:14:09 -0700

    commit

---
+++ Labs/LAB 3/LAB 3 TASK 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    printf("\nBefore swapping: a = %d, b = %d\n", a, b);
+
+    // Swapping logic
+    c = a;
+    a = b;
+    b = c;
+    printf("After swapping:  a = %d, b = %d\n", a, b);
+
+    return 0;
+}
